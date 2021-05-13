b4dccc8d7ffb89cb51d274c28462f6d650544577
daehyeon0121 <84111435+daehyeon0121@users.noreply.github.com>
2021-05-13 17:42:39 +0900

    [김대현]개발환영운영지원(프라임넘버 구하기)
    과제입니다

---
+++ primeNumber.java
@@ -0,0 +1,15 @@
+
+public class primeNumber {
+
+	public static void main(String[] args) {
+		
+		int num = 8;
+		System.out.println(num);
+		{
+			for(int i = 2; i <= num-2; i++);
+			
+			
+		}
+	
+	}
+	
